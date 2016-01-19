2f50bbe97a8f5698009fa14e4c8be8a42b74a425
Matthias Mees <mm@yellowled.de>
2016-01-19 12:54:40 +0100

    Add JS assets

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    // Smallscreen nav
+    var nav = responsiveNav('.nav-collapse', {
+        customToggle: "#open-nav",
+        closeOnNavClick: true,
+    });
+
+    // SVG polyfill
+    // svg4everybody();
+});
