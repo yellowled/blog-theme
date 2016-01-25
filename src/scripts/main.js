ee4a82a3a33fd6e6c489ea7c87b5dbd81fe6e3fc
Matthias Mees <mm@yellowled.de>
2016-01-25 16:37:40 +0100

    Remove service nav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,4 @@
 (function($) {
-    // Smallscreen nav
-    var nav = responsiveNav('.nav-collapse', {
-        customToggle: "#show-nav",
-        closeOnNavClick: true,
-    });
-
     // SVG polyfill
     svg4everybody();
 })(jQuery);
