1ffc442051bd8aeb88fd393e243fa0673330d595
Matthias Mees <mm@yellowled.de>
2016-02-05 10:23:24 +0100

    Add prismjs for syntax highlighting
    - Add prismjs via npm
    - Add prismjs + show-language plugin to JS build
    - Add language defs for Bash, PHP, SCSS + Smarty to JS build
    - Add prismjs's CSS as a SCSS module
    - Add CSS for show-language plugin
    
    Closes #2

--- config.js
+++ config.js
@@ -5,8 +5,14 @@ module.exports = {
     },
     concat: {
         src: [
-                    'node_modules/jquery-collapse/src/jquery.collapse.js',
                     'node_modules/svg4everybody/dist/svg4everybody.js',
+                    'node_modules/jquery-collapse/src/jquery.collapse.js',
+                    'node_modules/prismjs/prism.js',
+                    'node_modules/prismjs/plugins/show-language/prism-show-language.js',
+                    'node_modules/prismjs/components/prism-bash.js',
+                    'node_modules/prismjs/components/prism-php.js',
+                    'node_modules/prismjs/components/prism-scss.js',
+                    'node_modules/prismjs/components/prism-smarty.js',
                     'src/scripts/main.js'
               ],
         dest:       'src/scripts/master.js'
