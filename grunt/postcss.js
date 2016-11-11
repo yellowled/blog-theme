e0a59900e969909daf7b1fa19aa51f059a6625c5
Matthias Mees <mm@yellowled.de>
2016-11-11 23:34:00 +0100

    Add postcss-flexbug-fixes

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
                     cascade: false,
                     remove: true
                 }),
+                require('postcss-flexbugs-fixes')(),
                 require('css-mqpacker')({
                     sort: true
                 }),
