0746b23b752f66f1693d01fef57dfbec86fe85ad
Matthias Mees <mm@yellowled.de>
2018-06-29 21:47:11 +0200

    Update deps + devDeps, especially grunt-sass

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,8 +1,11 @@
 // Compile SCSS files to CSS
+const sass = require('node-sass');
+
 module.exports = function(grunt) {
     grunt.config('sass', {
         build: {
             options: {
+                implementation: sass,
                 outputStyle: 'nested',
                 precision: 4,
                 sourceMap: false
