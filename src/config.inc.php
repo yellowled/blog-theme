46c8907d006c19cfd03b3f34dcd14094518b53ae
Matthias Mees <mm@yellowled.de>
2018-12-11 22:39:53 +0100

    Update dependencies

--- src/config.inc.php
+++ src/config.inc.php
@@ -3,7 +3,7 @@ if (IN_serendipity !== true) { die ("Don't hack!"); }
 
 @serendipity_plugin_api::load_language(dirname(__FILE__));
 
-$serendipity['smarty']->assign('theme_version', '2.0.2');
+$serendipity['smarty']->assign('theme_version', '2.0.3');
 
 $serendipity['smarty']->assign(array('currpage'  => "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
                                      'currpage2' => $_SERVER['REQUEST_URI']));
